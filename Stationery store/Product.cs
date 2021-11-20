aaebfb098261a6f40ec163150eb15b4ff50f8e36
Василий Пранин <arullintalgat36@gmail.com>
2021-11-20 16:20:41 +0300

    ghgh

---
+++ Stationery store/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stationery_store
+{
+    class Product
+    {
+        public int paper { get; set; } = 20;
+        public int notebook { get; set; } = 50;
+        public int pen { get; set; } = 15;
+        public int pencil { get; set; } = 15;
+        public int eraser { get; set; } = 20;
+        
+    }
+}
